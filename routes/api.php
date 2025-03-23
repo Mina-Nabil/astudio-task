5f3a38e2bc883bc1a1924d09a32cafea513dec81
Mina Nabil <mina9492@hotmail.com>
2025-03-23 12:33:24 +0200

    adding base filters endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\FiltersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/jobs', [JobController::class, 'index']);
+
+Route::get('/filters/categories', [FiltersController::class, 'categories']);
+Route::get('/filters/locations', [FiltersController::class, 'locations']);
+Route::get('/filters/languages', [FiltersController::class, 'languages']);
+Route::get('/filters/attributes', [FiltersController::class, 'attributes']);
